901663a68f233e58660b2e88af1b835f25568c9e
ongdv <ongdv94@gmail.com>
2023-03-02 12:28:54 +0900

    [UPDATE] 엔티티 작성 및 수정
    - 엔티티 제약 조건 추가
    - Charge 엔티티 작성
    - Equipment 엔티티 작성

---
+++ src/entity/equipment.entity.ts
@@ -0,0 +1,48 @@
+import { Category, Project } from 'src/entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 't_equipment' })
+export class Equipment {
+  @PrimaryGeneratedColumn('uuid')
+  equipment_id: string;
+
+  @Column()
+  category_id: string;
+
+  @Column()
+  project_id: string;
+
+  @Column()
+  equipment_nm: string;
+
+  @Column()
+  equipment_desc: string;
+
+  @Column()
+  equipment_thumbnail: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  modified_at: Date;
+
+  @ManyToOne(() => Category, (category) => category.category_id, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  category: Category;
+
+  @ManyToOne(() => Project, (project) => project.project_id, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  project: Project;
+}
