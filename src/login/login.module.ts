9722a644e8c36b1c22402f5adaa7d78a53ba8b34
ongdv <ongdv94@gmail.com>
2023-03-02 17:01:51 +0900

    [UPDATE] docker-compose 수정
    - 린트 수정
    - docker-compose 수정
      docker-compose up시, config와 data, entrypoint 작성

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -7,6 +7,6 @@ import { LoginService } from './login.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Login])],
   controllers: [LoginController],
-  providers: [LoginService]
+  providers: [LoginService],
 })
-export class LoginModule { }
+export class LoginModule {}
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -6,107 +6,112 @@ import { CreateLoginDto, UpdateLoginDto } from './dto';
 
 @Injectable()
 export class LoginService {
-    constructor(
-        @InjectRepository(Login) private loginRepository: Repository<Login>
-    ) { }
+  constructor(
+    @InjectRepository(Login) private loginRepository: Repository<Login>
+  ) {}
 
-    /**
-     *  로그인 정보 생성
-     * --
-     * @param loginInfo
-     * @returns
-     */
-    async createLogin(loginInfo: CreateLoginDto) {
-        try {
-            const login_duration = new Date();
-            login_duration.setMinutes(login_duration.getMinutes() + 5)
-            const test = await this.loginRepository.create({
-                ...loginInfo,
-                login_duration
-            });
-            const result = await this.loginRepository.save(test);
-            return result;
-        } catch (e) {
-            throw e;
-        }
+  /**
+   *  로그인 정보 생성
+   * --
+   * @param loginInfo
+   * @returns
+   */
+  async createLogin(loginInfo: CreateLoginDto) {
+    try {
+      const login_duration = new Date();
+      login_duration.setMinutes(login_duration.getMinutes() + 5);
+      const test = await this.loginRepository.create({
+        ...loginInfo,
+        login_duration,
+      });
+      const result = await this.loginRepository.save(test);
+      return result;
+    } catch (e) {
+      throw e;
     }
+  }
 
-    /**
-     * 로그인 전체 목록 조회
-     * --
-     * @returns
-     */
-    async getLoginList() {
-        try {
-            const result = await this.loginRepository.find();
-            return result;
-        } catch (e) {
-            throw e;
-        }
+  /**
+   * 로그인 전체 목록 조회
+   * --
+   * @returns
+   */
+  async getLoginList() {
+    try {
+      const result = await this.loginRepository.find();
+      return result;
+    } catch (e) {
+      throw e;
     }
+  }
 
-    /**
-     * 로그인 정보 수정
-     * --
-     * @param loginInfo
-     * @returns
-     */
-    async updateLogin(loginInfo: UpdateLoginDto) {
-        try {
-            const { login_id, ...updateInfo } = loginInfo;
+  /**
+   * 로그인 정보 수정
+   * --
+   * @param loginInfo
+   * @returns
+   */
+  async updateLogin(loginInfo: UpdateLoginDto) {
+    try {
+      const { login_id, ...updateInfo } = loginInfo;
 
-            const result = await this.loginRepository.update({ login_id }, updateInfo);
-            return result;
-        } catch (e) {
-            throw e;
-        }
+      const result = await this.loginRepository.update(
+        { login_id },
+        updateInfo
+      );
+      return result;
+    } catch (e) {
+      throw e;
     }
+  }
 
-    /**
-     * 로그인 정보 삭제
-     * --
-     * @param login_id
-     * @returns
-     */
-    async deleteLogin(login_id: string) {
-        try {
-            await this.loginRepository.findOneOrFail({ where: { login_id } });
+  /**
+   * 로그인 정보 삭제
+   * --
+   * @param login_id
+   * @returns
+   */
+  async deleteLogin(login_id: string) {
+    try {
+      await this.loginRepository.findOneOrFail({ where: { login_id } });
 
-            const result = await this.loginRepository.delete({ login_id });
-            return result;
-        } catch (e) {
-            throw e;
-        }
+      const result = await this.loginRepository.delete({ login_id });
+      return result;
+    } catch (e) {
+      throw e;
     }
+  }
 
-    /**
-     * 로그인 상세 정보 조회
-     * @param login_id
-     * @returns
-     */
-    async getLoginInfo(login_id: string) {
-        try {
-            const user = await this.loginRepository.findOneOrFail({
-                where: { login_id: login_id },
-            });
-            return user;
-        } catch (e) {
-            throw e;
-        }
+  /**
+   * 로그인 상세 정보 조회
+   * @param login_id
+   * @returns
+   */
+  async getLoginInfo(login_id: string) {
+    try {
+      const user = await this.loginRepository.findOneOrFail({
+        where: { login_id: login_id },
+      });
+      return user;
+    } catch (e) {
+      throw e;
     }
+  }
 
-    /**
-     * 유저별 로그인 정보 조회
-     * --
-     * @param user_id
-     * @returns
-     */
-    async getUserLoginInfo(user_id: string) {
-        try {
-            const result = await this.loginRepository.find({ where: { user_id: user_id } });
-            return result;
-        } catch (e) {
-            throw e;
-        }
+  /**
+   * 유저별 로그인 정보 조회
+   * --
+   * @param user_id
+   * @returns
+   */
+  async getUserLoginInfo(user_id: string) {
+    try {
+      const result = await this.loginRepository.find({
+        where: { user_id: user_id },
+      });
+      return result;
+    } catch (e) {
+      throw e;
     }
+  }
 }
