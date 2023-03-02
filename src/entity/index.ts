b847a7e3d4fac9a9112277c13d6c5a8c39605db3
kyon48 <dygks1733@pusan.ac.kr>
2023-03-02 16:23:37 +0900

    create login entity and CRUD

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,6 +1,8 @@
+export * from './category.entity';
+export * from './charge.entity';
+export * from './equipment.entity';
+export * from './login.entity';
+export * from './project.entity';
 export * from './team.entity';
 export * from './user.entity';
-export * from './category.entity';
-export * from './project.entity';
-export * from './equipment.entity';
-export * from './charge.entity';
+
