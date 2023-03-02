3a7534e8d049c8d388e6a1622b71136181eb4059
ongdv <ongdv94@gmail.com>
2023-03-02 09:37:46 +0900

    [UPDATE] 자잘한 교정
    - DTO에서 IsOptional 어노테이션 사용시, 필드에도 옵셔널 표시
    - 린트 적용

--- src/entity/project.entity.ts
+++ src/entity/project.entity.ts
@@ -1,19 +1,25 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
-@Entity({name: 't_project'})
-export class Project{
-    @PrimaryGeneratedColumn('uuid')
-    project_id: string;
+@Entity({ name: 't_project' })
+export class Project {
+  @PrimaryGeneratedColumn('uuid')
+  project_id: string;
 
-    @Column()
-    project_title:string;
+  @Column()
+  project_title: string;
 
-    @Column()
-    project_desc: string;
+  @Column()
+  project_desc: string;
 
-    @CreateDateColumn()
-    created_at: Date;
+  @CreateDateColumn()
+  created_at: Date;
 
-    @UpdateDateColumn()
-    modified_at: Date;
+  @UpdateDateColumn()
+  modified_at: Date;
 }
--- src/project/dto/update-project.dto.ts
+++ src/project/dto/update-project.dto.ts
@@ -1,16 +1,15 @@
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
-export class UpdateProjectDto{
-    @IsString()
-    @IsNotEmpty()
-    project_id: string;
+export class UpdateProjectDto {
+  @IsString()
+  @IsNotEmpty()
+  project_id: string;
 
-    @IsString()
-    @IsOptional()
-    project_title: string;
+  @IsString()
+  @IsOptional()
+  project_title?: string;
 
-    @IsString()
-    @IsOptional()
-    project_desc: string;
-
+  @IsString()
+  @IsOptional()
+  project_desc?: string;
 }
