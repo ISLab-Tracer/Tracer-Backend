0fc1ee28521ea0cd3e490258459d4e80d389fe84
ongdv <ongdv@pusan.ac.kr>
2023-03-29 14:51:52 +0900

    [UPDATE] Single File Update
    - 단일 파일 업로드 추가

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -7,10 +7,11 @@ import {
   Bind,
   UploadedFiles,
   Body,
+  UploadedFile,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { Response } from 'express';
-import { FilesInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor, FileInterceptor } from '@nestjs/platform-express';
 import { multerMemoryOptions } from 'src/config/multer.options';
 import { UploadDto } from './dto/upload.dto';
 
@@ -19,9 +20,32 @@ export class UploadController {
   constructor(private uploadService: UploadService) {}
 
   @Post()
+  @UseInterceptors(FileInterceptor('file', multerMemoryOptions))
+  @Bind(UploadedFile())
+  async singleUpload(
+    @Res() res: Response,
+    file: Express.Multer.File,
+    @Body() uploadDto: UploadDto
+  ) {
+    try {
+      const result = await this.uploadService.singleUpload(file, uploadDto);
+      return res.status(HttpStatus.OK).json({
+        status: HttpStatus.OK,
+        message: 'success',
+        data: result,
+      });
+    } catch (e) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        status: HttpStatus.BAD_REQUEST,
+        message: e.message,
+      });
+    }
+  }
+
+  @Post('/multi')
   @UseInterceptors(FilesInterceptor('files', null, multerMemoryOptions))
   @Bind(UploadedFiles())
-  async uploadFiles(
+  async multiUpload(
     @Res() res: Response,
     files: Express.Multer.File[],
     @Body() uploadDto: UploadDto
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -6,6 +6,13 @@ import { uploadFileURL } from 'src/config/multer.options';
 @Injectable()
 export class UploadService {
   constructor() {}
+  /**
+   * 파일 여러개 업로드
+   * --
+   * @param files
+   * @param uploadDto
+   * @returns
+   */
   async multiFileUpload(files: Express.Multer.File[], uploadDto: UploadDto) {
     try {
       const { upload_path, file_name } = uploadDto;
@@ -15,13 +22,46 @@ export class UploadService {
       }
 
       const result = files.map((file) => {
-        this.uploadFile(file, upload_path, file_name);
+        return this.uploadFile(file, upload_path, file_name);
       });
+
+      return result;
     } catch (e) {
       throw e;
     }
   }
 
+  /**
+   * 단일 파일 업로드
+   * --
+   * @param file
+   * @param uploadDto
+   * @returns
+   */
+  async singleUpload(file: Express.Multer.File, uploadDto: UploadDto) {
+    try {
+      const { upload_path, file_name } = uploadDto;
+
+      if (!fs.existsSync(upload_path)) {
+        fs.mkdirSync(upload_path);
+      }
+      const result = this.uploadFile(file, upload_path, file_name);
+
+      return result;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  /**
+   * 파일 업로드
+   * --
+   * 메모리에 저장된 파일을 실제 파일로 만들고, URL을 return 해주는 메서드
+   * @param file
+   * @param uploadFilePath
+   * @param fileName
+   * @returns
+   */
   uploadFile(
     file: Express.Multer.File,
     uploadFilePath: string,
